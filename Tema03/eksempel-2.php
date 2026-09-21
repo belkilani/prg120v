825826bd84e440153a7c3a99d7be2329c6a39c0a
belkilani <hossam.belkilani@gmail.com>
2026-09-21 14:13:12 +0200

    Add PHP script to print numbers 1 to 10
    This script prints numbers from 1 to 10, each on a new line, using a while loop.

---
+++ Tema03/eksempel-2.php
@@ -0,0 +1,20 @@
+<?php     /* Eksempel 2 */
+/*
+/*    Programmet skriver ut tallene fra 1 til 10 på hver sin linje
+/*    Eksempelet viser bruk av while-setning
+*/
+  $tall=1;  
+
+  while ($tall<=10)
+    {
+      print("$tall <br/>"); 
+      $tall++;  
+    }
+
+/*
+startverdi: $tall=1 
+betingelse: $tall<=10
+økning: $tall++ (dette er det samme som $tall=$tall+1)
+*/
+
+?>
